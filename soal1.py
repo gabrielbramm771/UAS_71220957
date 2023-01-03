aefa6a673aa4ed96b1a2dba55d8c133b307a6eb2
gabrielbramm771 <117884203+gabrielbramm771@users.noreply.github.com>
2023-01-03 12:24:15 +0700

    Add files via upload

---
+++ soal1.py
@@ -0,0 +1,13 @@
+print("******* Kredit Keaktifan Mahasiswa *******")
+print("(Student Activities Credit) ")
+
+if choice == "1":
+    pilihan 1 = input("1.Menambahkan Kegiatan")
+    pilihan 2 = input("2.Menampilkan Jumlah poin")
+    pilihan 3 = input("keluar")
+
+print()
+print=("------------------")
+    elif == int("Silahkan masukan pilihan anda:")
+        print= ("Nama Kegiatan: HMTI 2022")
+        print= ("Pilihan Kategori Kegiatan:")
---
+++ soal2.py
@@ -0,0 +1,107 @@
+import turtle
+t = turtle.Turtle ()
+
+turtle.bgcolor("pink")
+t.pensize(3)
+t.color ("yellow")
+
+t.color("yellow")
+t.begin_fill()
+t.penup()
+t.goto(-70,180)
+t.pendown()
+t.forward (180)
+t.right (45)
+t.forward(60)
+t.right(45)
+t.forward(180)
+t.right(45)
+t.forward(60)
+t.right(45)
+t.forward(180)
+t.right(45)
+t.forward(60)
+t.right(45)
+t.forward(180)
+t.right(45)
+t.forward(60)
+t.right(45)
+t.penup()
+t.end_fill()
+
+t.color("blue")
+t.goto(30,40)
+t.pendown()
+t.circle(60)
+t.penup()
+
+t.color("red")
+t.goto (30,50)
+t.pendown()
+t.circle (20)
+t.penup ()
+
+t.goto (-60,-10)
+t.pendown()
+t.right (90)
+t.forward(50)
+t.penup()
+t.goto(-65,-35)
+t.pendown()
+t.left(90)
+t.forward(50)
+t.penup()
+t.goto(-20,-10)
+t.pendown()
+t.right(90)
+t.forward(50)
+t.penup()
+t.goto(10,-20)
+t.right(90)
+t.pendown()
+t.right(90)
+t.backward(50)
+t.penup()
+t.goto(11,-11)
+t.pendown()
+t.penup()
+t.goto(50,-11)
+t.pendown()
+t.backward(30)
+t.penup()
+t.goto(50,-50)
+t.pendown()
+t.circle(3)
+t.penup()
+
+t.color("red")
+t.begin_fill()
+t.goto(-5,-120)
+t.pendown()
+t.forward(25)
+t.right(90)
+t.forward(25)
+t.right(90)
+t.forward(90)
+t.right(90)
+t.forward(25)
+t.right(90)
+t.forward(75)
+t.end_fill()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+turtle.done
+
